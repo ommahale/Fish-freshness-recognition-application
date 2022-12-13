938385783e677ceeef93f85d69ed8f4ad117038b
OM MAHALE <78585854+ommahale@users.noreply.github.com>
2022-12-13 10:35:18 +0530

    API files

---
+++ API/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+userInfo={'data':[
+        {
+            'userID':1,
+            'name':'Om'
+        },
+        {
+            'userID':2,
+            'name':'Yash'
+        },
+        {
+            'userID':3,
+            'name':'Kundan'
+        }
+    ]}
+
+@app.get('/')
+def index():
+    return userInfo
+
+
