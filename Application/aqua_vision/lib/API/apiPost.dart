1a2c950b3a3da62601a3b0a42345fe4f6ca94991
OM MAHALE <78585854+ommahale@users.noreply.github.com>
2022-12-14 21:56:53 +0530

    Completed API Post request file

--- Application/aqua_vision/lib/API/apiPost.dart
+++ Application/aqua_vision/lib/API/apiPost.dart
@@ -1,18 +1,31 @@
-import 'dart:html';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:http_parser/http_parser.dart';
 
 class Classifier {
-  File? image;
-  late Map output;
-
-  Classifier({this.image});
-  Future<void> getData() async {
+  Future<dynamic> getData(File image, String filename) async {
     try {
+      Map<String, String> header = {'Content-type': 'multipart/form-data'};
       var request = http.MultipartRequest(
           'POST', Uri.parse('http://localhost:8000/upload'));
+      request.files.add(http.MultipartFile(
+        'image',
+        image.readAsBytes().asStream(),
+        image.lengthSync(),
+        filename: filename,
+        contentType: MediaType('image', 'jpeg'),
+      ));
+      request.headers.addAll(header);
+      StreamedResponse res = await request.send();
+      print(http.Response.fromStream(res));
+      Map<dynamic, dynamic> output =
+          jsonDecode(http.Response.fromStream(res).toString());
+      return output;
     } catch (e) {
-      output = {'error': e, 'label': 'Unable to connect'};
+      return {'error': e, 'label': 'Unable to connect'};
     }
   }
 }
