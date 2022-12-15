55b42cacada06cafc9dff28d3ea690754219389d
OM MAHALE <78585854+ommahale@users.noreply.github.com>
2022-12-15 11:51:33 +0530

    Added Dio and made a post request

--- Application/aqua_vision/lib/API/apiPost.dart
+++ Application/aqua_vision/lib/API/apiPost.dart
@@ -1,31 +1,49 @@
-import 'dart:convert';
 import 'dart:io';
-
-import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
-import 'package:http_parser/http_parser.dart';
+import 'package:dio/dio.dart';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:http/http.dart';
+// import 'package:http_parser/http_parser.dart';
 
 class Classifier {
+  //Using http
+
+  // Future<dynamic> getData(File image, String filename) async {
+  //   try {
+  //     Map<String, String> header = {'Content-type': 'multipart/form-data'};
+  //     var request = http.MultipartRequest(
+  //         'POST', Uri.parse('http://localhost:8000/upload'));
+  //     request.files.add(http.MultipartFile(
+  //       'image',
+  //       image.readAsBytes().asStream(),
+  //       image.lengthSync(),
+  //       filename: filename,
+  //       contentType: MediaType('image', 'jpeg'),
+  //     ));
+  //     request.headers.addAll(header);
+  //     StreamedResponse res = await request.send();
+  //     print(http.Response.fromStream(res));
+  //     Map<dynamic, dynamic> output =
+  //         jsonDecode(http.Response.fromStream(res).toString());
+  //     return output;
+  //   } catch (e) {
+  //     return {'error': e, 'label': 'Unable to connect'};
+  //   }
+  // }
+
+  //Using dio
+
   Future<dynamic> getData(File image, String filename) async {
     try {
-      Map<String, String> header = {'Content-type': 'multipart/form-data'};
-      var request = http.MultipartRequest(
-          'POST', Uri.parse('http://localhost:8000/upload'));
-      request.files.add(http.MultipartFile(
-        'image',
-        image.readAsBytes().asStream(),
-        image.lengthSync(),
-        filename: filename,
-        contentType: MediaType('image', 'jpeg'),
-      ));
-      request.headers.addAll(header);
-      StreamedResponse res = await request.send();
-      print(http.Response.fromStream(res));
-      Map<dynamic, dynamic> output =
-          jsonDecode(http.Response.fromStream(res).toString());
-      return output;
+      Dio dio = Dio();
+      var formData = FormData();
+      formData.files
+          .add(MapEntry('image', await MultipartFile.fromFile(image.path)));
+      var respose = await dio.post('http://127.0.0.1:8000', data: image);
+      print(respose.data.toString());
+      return respose.data;
     } catch (e) {
-      return {'error': e, 'label': 'Unable to connect'};
+      return {'label': 'error has occured'};
     }
   }
 }
